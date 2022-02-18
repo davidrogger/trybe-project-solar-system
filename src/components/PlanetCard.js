91b88d818a309c1d0cb8706dc719b9d8aa1e7686
Davíd Roggér <davidrogger.a@gmail.com>
2022-02-18 16:18:35 -0300

    estilização para melhor visualização dos elementos

--- src/components/PlanetCard.js
+++ src/components/PlanetCard.js
@@ -7,8 +7,8 @@ class PlanetCard extends Component {
 
     return (
       <div data-testid="planet-card">
-        <p data-testid="planet-name">{planetName}</p>
         <img src={ planetImage } alt={ `Planeta ${planetName}` } />
+        <p data-testid="planet-name">{planetName}</p>
       </div>
     );
   }
