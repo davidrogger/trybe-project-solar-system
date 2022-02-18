4e52fc594d4311862b4816b1a788dd0bc32f2095
Davíd Roggér <davidrogger.a@gmail.com>
2022-02-18 18:18:27 -0300

    atualizacao de estilo do display de planetas

--- src/components/PlanetCard.js
+++ src/components/PlanetCard.js
@@ -6,7 +6,7 @@ class PlanetCard extends Component {
     const { planetName, planetImage } = this.props;
 
     return (
-      <div data-testid="planet-card">
+      <div data-testid="planet-card" className="planet-card">
         <img src={ planetImage } alt={ `Planeta ${planetName}` } />
         <p data-testid="planet-name">{planetName}</p>
       </div>
