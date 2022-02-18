651a0c5406f2f3c75d52f578c23e7eb8f2b1f7bb
Davíd Roggér <davidrogger.a@gmail.com>
2022-02-18 18:46:38 -0300

    atualizando estilos de missioes

--- src/components/MissionCard.js
+++ src/components/MissionCard.js
@@ -8,6 +8,7 @@ class MissionCard extends Component {
     return (
       <div data-testid="mission-card" className="mission-card">
         <p data-testid="mission-name">{name}</p>
+        <hr />
         <p data-testid="mission-year">{year}</p>
         <p data-testid="mission-country">{country}</p>
         <p data-testid="mission-destination">{destination}</p>
